71a10b5f4f54a51e75e9d6ad536cee636cd322a3
loyd <loyd.fassi@gmail.com>
2024-01-20 18:50:14 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// primitifs
+const string = "hello world "
+const number = 15
+const boolean = true
+
+// array
+const randomArray = [
+    "hello",
+    "hello 2 ",
+]
+
+//object
+const valentin = { 
+    "name": "valentin",
+    "age": 23,
+}
+
+//fonction
+function estilmajeur(param1) {
+    if (param1.age > 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
